c3a224e226343b868e35d1be6209fb35420d42de
EdgedSquirrels <71828321+EdgedSquirrels@users.noreply.github.com>
2022-08-16 11:50:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def main():
     
     # EventManager listen to events and notice model, controller, view
     ev_manager = EventManager()
-    model      = GameEngine(ev_manager)
+    model = GameEngine(ev_manager)
     controller = Controller(ev_manager, model)
-    view       = GraphicalView(ev_manager, model)
+    view = GraphicalView(ev_manager, model)
 
     # Main loop
     model.run()
